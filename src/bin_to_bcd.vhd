d0ddc17dd2ce8d22cf929141f9f9217ebd20d2ef
joaoporcino <joaomarcos_cp@hotmail.com>
2025-12-09 15:54:17 -0300

    feat: acrescentado a porta de enable

--- src/bin_to_bcd.vhd
+++ src/bin_to_bcd.vhd
@@ -1,27 +1,30 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 
-entity bin_to_bcd is
-    Port (
-        bin_in  : in  STD_LOGIC_VECTOR(6 downto 0); 
-        dezena  : out STD_LOGIC_VECTOR(3 downto 0);
-        unidade : out STD_LOGIC_VECTOR(3 downto 0)
+ENTITY bin_to_bcd IS
+    PORT (
+        enable : IN STD_LOGIC;
+        bin_in : IN STD_LOGIC_VECTOR(6 DOWNTO 0);
+        dezena : OUT STD_LOGIC_VECTOR(3 DOWNTO 0);
+        unidade : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
-end bin_to_bcd;
+END bin_to_bcd;
 
-architecture Behavioral of bin_to_bcd is
-    signal bin_unsigned : unsigned(6 downto 0);
-    signal dezena_temp  : unsigned(6 downto 0);
-    signal unidade_temp : unsigned(6 downto 0);
-begin
-    bin_unsigned <= unsigned(bin_in);
-    
-    dezena_temp <= bin_unsigned / 10;
-    
-    unidade_temp <= bin_unsigned mod 10;
-    
-    dezena  <= std_logic_vector(dezena_temp(3 downto 0));
-    unidade <= std_logic_vector(unidade_temp(3 downto 0));
-    
-end Behavioral;
+ARCHITECTURE Behavioral OF bin_to_bcd IS
+    SIGNAL bin_unsigned : unsigned(6 DOWNTO 0);
+    SIGNAL dezena_temp : unsigned(6 DOWNTO 0);
+    SIGNAL unidade_temp : unsigned(6 DOWNTO 0);
+BEGIN
+    IF enable = 1 THEN
+        bin_unsigned <= unsigned(bin_in);
+
+        dezena_temp <= bin_unsigned / 10;
+
+        unidade_temp <= bin_unsigned MOD 10;
+
+        dezena <= STD_LOGIC_VECTOR(dezena_temp(3 DOWNTO 0));
+        unidade <= STD_LOGIC_VECTOR(unidade_temp(3 DOWNTO 0));
+    END IF;
+
+END Behavioral;
